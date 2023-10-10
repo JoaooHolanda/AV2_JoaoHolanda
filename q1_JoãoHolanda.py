7039758c2daa0d924eb8f253412a4ab242338624
João Holanda <lucasholanda11@hotmail.com>
2023-10-09 23:15:24 -0300

    q1 solve with sucess

--- q1_JoãoHolanda.py
+++ q1_JoãoHolanda.py
@@ -8,48 +8,69 @@ getuser = lambda: input("Qual seu nome? ")
 getpassword = lambda: input("Qual sua senha? ")
 blocked = lambda: print("Acesso bloqueado. Usuário não existente ou senha inválida")
 allowed = lambda: print("Acesso Permitido")
-nome = lambda user: user
-cond = lambda user, gpassword: user["senha"] == gpassword
-Tela = lambda user: print(f"Olá, {nome(user)}! Escolha qual opção você deseja:\n1. Extrato Bancário\n2. Saque Bancário\n3. Depósito Bancário\nDigite 'Sim' para selecionar uma opção ou 'Não' para sair: ")
-
 # Obtenha o nome do usuário uma vez e armazene em uma variável
 nome_do_usuario = getuser()
 
 # Obtenha a senha do usuário uma vez e armazene em uma variável
 senha_do_usuario = getpassword()
 
+nome = lambda nome_u: nome_u
+# Verifica se o usuário existe no dicionário usando uma função lambda
+cond = lambda user, gpassword: users.get(user, {}).get("senha") == gpassword
+
+Tela = lambda user: print(f"Olá, {nome(user)}! Escolha qual opção você deseja:\n1. Extrato Bancário\n2. Saque Bancário\n3. Depósito Bancário\nDigite 'Sim' para selecionar uma opção ou 'Não' para sair: ")
+
 # Faça o login e obtenha o usuário
-login = lambda users, guser, gpassword, Tela, blocked, allowed: users.get(guser) if cond(users.get(guser), gpassword) else None
+login = lambda users, guser, gpassword, Tela, blocked, allowed: (allowed() or Tela(guser) or users.get(guser))  if cond(guser, gpassword) else blocked()
 
 usuario_logado = login(users, nome_do_usuario, senha_do_usuario, Tela, blocked, allowed)
 
+# Lambda para saque
+sacar = lambda usuario: (
+    valor := float(input("Quanto você deseja Sacar? R$ ")),
+    print(f"Você selecionou Saque Bancário. Executando ação..."),
+    (print('Saldo Insuficiente para saque!') if valor > usuario['saldo']
+     else (
+         (lambda: usuario.__setitem__('saldo', usuario['saldo'] - valor))(),
+         print(f"{nome_do_usuario} Valor de R$ {valor:.2f} sacado com sucesso\nSeu novo Extrato é de R$ {usuario['saldo']:.2f}")
+     ))[1] if valor > 0 else print("Valor de saque inválido.")
+)
+
+# Lambda para depósito
+depositar = lambda usuario: (
+    valor := float(input("Quanto você deseja Depositar? R$ ")),
+    print(f"Você selecionou Depósito Bancário. Executando ação..."),
+    (print("Valor de depósito inválido.") if valor <= 0
+     else (
+         (lambda: usuario.__setitem__('saldo', usuario['saldo'] + valor))(),
+         print(f"{nome_do_usuario} Valor de R$ {valor:.2f} depositado com sucesso\nSeu novo Extrato é de R$ {usuario['saldo']:.2f}")
+     ))[1] if valor > 0 else print("Valor de depósito inválido.")
+)
+
+# Lambda para sair
+sair = lambda usuario: (
+    choose := input("Deseja sair? (Sim/Não): ").strip().lower(),
+    (exit() if choose == "sim" else None) if choose == "sim" else None
+)
+
+
+
 while usuario_logado:
-    # Após o login bem-sucedido, obter a escolha do usuário
+    # Após o login bem-sucedido, obtenha a escolha do usuário
     opcao = input("Escolha uma opção (1, 2 ou 3) ou deseja sair (digitar sair): ").strip().lower()
     
-    if opcao == "1":
-        print("Você selecionou Extrato Bancário. Executando ação...")
-        print(f"{nome_do_usuario} Você possui: R$ {usuario_logado['saldo']:.2f}")
-    elif opcao == "2":
-        print("Você selecionou Saque Bancário. Executando ação...")
-        valor = float(input("Quanto você deseja Sacar? R$ "))
-        # Verifica saldo
-        if valor > usuario_logado['saldo']:
-            print('Saldo Insuficiente para saque!')
-        else:
-            usuario_logado['saldo'] -= valor
-            print(f"{nome_do_usuario} Valor de R$ {valor:.2f} sacado com sucesso\nSeu novo Extrato é de R$ {usuario_logado['saldo']:.2f}")   
-    elif opcao == "3":
-        print("Você selecionou Depósito Bancário. Executando ação...")
-        valor = float(input("Quanto você deseja Depositar? R$ "))
-        if valor <= 0:
-            print("Valor de depósito inválido.")
-        else:
-            usuario_logado['saldo'] += valor
-            print(f"{nome_do_usuario} Valor de R$ {valor:.2f} depositado com sucesso\nSeu novo Extrato é de R$ {usuario_logado['saldo']:.2f}")
-    elif opcao == "sair":
-        choose = input("Deseja sair? (Sim/Não): ").strip().lower()
-        if choose == "sim":
-            break
+    # Usando list comprehension para mapear opções para funções correspondentes
+    acoes = {
+        '1': (lambda: print(f"{nome_do_usuario} Você possui: R$ {usuario_logado['saldo']:.2f}"), "Extrato Bancário"),
+        '2': (lambda: sacar(usuario_logado), "Saque Bancário"),
+        '3': (lambda: depositar(usuario_logado), "Depósito Bancário"),
+        'sair': (lambda: sair(usuario_logado), "Sair"),
+    }
+
+    # Executa a ação correspondente à opção ou imprime "Opção inválida"
+    acao = acoes.get(opcao)
+    if acao:
+        acao[0]()  # Chama a função correspondente
+        print(f"Você selecionou {acao[1]}. Executando ação...")
     else:
         print("Opção inválida.")
