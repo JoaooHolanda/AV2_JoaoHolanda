d5d0f9c131a81a1bbc0ae79c92c2b21c6c1a955d
João Holanda <lucasholanda11@hotmail.com>
2023-10-12 08:10:38 -0300

    solving duplicated func

--- q4_JoãoHolanda/q4_JoãoHolanda.py
+++ q4_JoãoHolanda/q4_JoãoHolanda.py
@@ -12,7 +12,6 @@ execsqlcmd = lambda cmd, crs: crs.execute (cmd)
 
 execcreatetable = lambda table, attrs, crs : execsqlcmd("CREATE TABLE IF NOT EXISTS  " + table + " (" + attrs + ");\n", crs)
 execcreatedatabase = lambda dbname, crs: execsqlcmd("CREATE DATABASE IF NOT EXISTS   " + dbname + ";\n", crs)
-execcreatedatabase = lambda dbname, crs: execsqlcmd("CREATE DATABASE IF NOT EXISTS  " + dbname + ";\n", crs)
 execdropdatabase = lambda dbname, crs : execsqlcmd ("DROP DATABASE " + dbname + ";\n", crs)
 execdroptable = lambda dbname, crs : execsqlcmd ("DROP TABLE " + dbname + ";\n", crs)
 execusedatabase = lambda dbname, crs : execsqlcmd ("USE " + dbname + ";\n", crs)
